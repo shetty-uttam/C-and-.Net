4b312dbc1db3579e0606d5c3efcb5e05822892bb
shetty-uttam <us185383@gmail.com>
2024-06-21 15:21:06 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter the size of the array: ");
+        int size = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[size];
+
+        Console.WriteLine($"Enter {size} elements:");
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write($"Enter element {i + 1}: ");
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        InsertionSort(arr);
+
+        Console.WriteLine("Sorted array:");
+        foreach (int num in arr)
+        {
+            Console.Write( " "+num);
+           Console.ReadLine();
+
+        }
+    }
+
+    public static void InsertionSort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 1; i < n; i++)
+        {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key)
+            {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+
+        Console.ReadLine();
+    }
+}
