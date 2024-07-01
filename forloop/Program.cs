207d6fb5a7650d0e7d60286b7a55fb9497d04717
shetty-uttam <us185383@gmail.com>
2024-07-01 16:04:32 +0530

    Add files via upload

---
+++ forloop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+
+        // Example of a for loop that calculates the sum of numbers from 1 to 10
+        int sum = 0;
+        for (int j = 1; j <= 10; j++)
+        {
+            sum += j; // Equivalent to: sum = sum + j;
+        }
+        Console.WriteLine("Sum of numbers from 1 to 10: " + sum);
+            Console.ReadLine();
+        }
+    }
+    
